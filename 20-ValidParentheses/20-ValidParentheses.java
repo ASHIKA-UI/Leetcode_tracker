1f740b09b427736f0dd9434727c34756338b7191
ASHIKA-UI <ashikababu5@gmail.com>
2026-07-21 09:02:29 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,43 @@
+// Last updated: 7/21/2026, 9:02:26 AM
+import java.util.Stack;
+
+public class Solution {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s1 = "()";
+        String s2 = "()[]{}";
+        String s3 = "(]";
+        String s4 = "([])";
+        String s5 = "([)]";
+
+        System.out.println(isValid(s1)); 
+        System.out.println(isValid(s2)); 
+        System.out.println(isValid(s3)); 
+        System.out.println(isValid(s4)); 
+        System.out.println(isValid(s5)); 
+    }
+}
