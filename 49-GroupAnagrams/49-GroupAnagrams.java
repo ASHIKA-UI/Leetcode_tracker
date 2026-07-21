2130a2cb5cbc08c67cab69c2fcaa485f4491699f
ASHIKA-UI <ashikababu5@gmail.com>
2026-07-21 09:02:15 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -0,0 +1,17 @@
+// Last updated: 7/21/2026, 9:02:12 AM
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String word: strs){
+            char[] c = word.toCharArray();
+            Arrays.sort(c);
+            String s = new String(c);
+
+            if(!map.containsKey(s)){
+                 map.put(s, new ArrayList<>());
+            } 
+            map.get(s).add(word);
+        } 
+        return new ArrayList<>(map.values());
+    }
+}
