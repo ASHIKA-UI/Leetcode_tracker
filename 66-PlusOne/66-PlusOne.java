8b9223092df3b2cbb37fc738b6d04167e9150b7c
ASHIKA-UI <ashikababu5@gmail.com>
2026-07-21 09:02:10 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.java
@@ -0,0 +1,30 @@
+// Last updated: 7/21/2026, 9:02:07 AM
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] digits1 = {1, 2, 3};
+        int[] digits2 = {4, 3, 2, 1};
+        int[] digits3 = {9};
+
+        System.out.println(Arrays.toString(plusOne(digits1)));
+        System.out.println(Arrays.toString(plusOne(digits2))); 
+        System.out.println(Arrays.toString(plusOne(digits3))); 
+    }
+}
