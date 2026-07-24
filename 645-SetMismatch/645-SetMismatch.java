845a5bb2df04508a5c661892a82d965a209ba5f1
ASHIKA-UI <ashikababu5@gmail.com>
2026-07-24 10:07:16 +0530

    File updated at 7/24/2026, 10:07:11 AM

--- 645-SetMismatch/645-SetMismatch.java
+++ 645-SetMismatch/645-SetMismatch.java
@@ -1,21 +1,21 @@
-// Last updated: 7/21/2026, 8:59:51 AM
-class Solution {
-    public int[] findErrorNums(int[] nums) {
-        int n = nums.length;
-        int[] freq = new int[n + 1];
-
-        for (int num : nums) {
-            freq[num]++;
-        }
-
-        int duplicate = -1, missing = -1;
-
-        
-        for (int i = 1; i <= n; i++) {
-            if (freq[i] == 2) duplicate = i;
-            if (freq[i] == 0) missing = i;
-        }
-
-        return new int[]{duplicate, missing};
-    }
-}
+// Last updated: 7/24/2026, 10:07:11 AM
+1class Solution {
+2    public int[] findErrorNums(int[] nums) {
+3        int n = nums.length;
+4        int[] freq = new int[n + 1];
+5
+6        for (int num : nums) {
+7            freq[num]++;
+8        }
+9
+10        int duplicate = -1, missing = -1;
+11
+12        
+13        for (int i = 1; i <= n; i++) {
+14            if (freq[i] == 2) duplicate = i;
+15            if (freq[i] == 0) missing = i;
+16        }
+17
+18        return new int[]{duplicate, missing};
+19    }
+20}
